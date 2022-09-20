bce090fa5d40b106c09713b2eff9ecd4a1e5eb3c
Eric Furrevig <efurrevig@gmail.com>
2022-09-20 19:10:53 -0400

    pieces.rb

---
+++ lib/pieces.rb
@@ -0,0 +1,51 @@
+class Piece
+
+    def initialize
+        @color = nil
+        @active = true
+    end
+
+
+end
+
+class Pawn < Piece
+
+    def initialize
+    end
+
+end
+
+class Knight < Piece
+
+    def initialize
+    end
+
+end
+
+class Bishop < Piece
+
+    def initialize
+    end
+    
+end
+
+class Rook < Piece
+
+    def initialize
+    end
+    
+end
+
+class Queen < Piece
+
+    def initialize
+    end
+    
+end
+
+class King < Piece
+
+    def initialize
+    end
+    
+end 

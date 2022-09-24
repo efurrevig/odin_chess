a3a9d987c8cf212dd1298156e4e00e8bcbb7655a
Eric Furrevig <efurrevig@gmail.com>
2022-09-24 16:47:50 -0400

    added piece movement without validation

--- lib/board.rb
+++ lib/board.rb
@@ -20,8 +20,8 @@ class Board
         @black_piece_symbols = {"King" => "\u2654", 
                                 "Queen" => "\u2655",
                                 "Rook" => "\u2656",
-                                "Bishop" => "\u2658",
-                                "Knight" => "\u2659",
+                                "Bishop" => "\u2657",
+                                "Knight" => "\u2658",
                                 "Pawn" => "\u2659"}
         @white_piece_symbols = {"King" => "\u265A", 
                                 "Queen" => "\u265B",
@@ -52,6 +52,26 @@ class Board
         @board = arr
     end
 
+    def validate_move(piece, move = [])
+
+    end
+
+    def get_piece_at_position(pos = [])
+        r, c = pos
+        return @board[r][c]
+    end
+
+    def move_piece(piece, move = [])
+        #set pieces current position on board to 0
+        r, c = piece.pos
+        board[r][c] = 0
+        #set piece.pos to new position
+        piece.pos = move
+        #set pieces new position on board
+        r, c = move
+        @board[r][c] = piece
+    end
+
     def print_board
         for r in 0...@board.length
             puts ''
@@ -74,47 +94,9 @@ end
 
 board = Board.new
 board.create_board
-# puts "#{board.board[0][0].type}"
+board.print_board
+piece = board.get_piece_at_position([0,0])
+board.move_piece(piece, [3,0])
 board.print_board
 
 
-
-
-
-
-
-
-
-# def create_board
-#     arr = []
-#     start = ['Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rook']
-#     white_pieces = {"King" => "\u2654", 
-#         "Queen" => "\u2655",
-#         "Rook" => "\u2656",
-#         "Bishop" => "\u2658",
-#         "Knight" => "\u2659",
-#         "Pawn" => "\u2659"}
-#     black_pieces = {"King" => "\u265A", 
-#         "Queen" => "\u265B",
-#         "Rook" => "\u265C",
-#         "Bishop" => "\u265D",
-#         "Knight" =>  "\u265E",
-#         "Pawn" => "\u265F"}
-#     for r in 0..7
-#         arr << Array.new
-#         for c in 0..7
-#             if r == 0
-#                 arr[r] << black_pieces[start[c]]
-#             elsif r == 1
-#                 arr[r] << black_pieces["Pawn"]
-#             elsif r == 7
-#                 arr[r] << white_pieces[start[c]]
-#             elsif r == 6
-#                 arr[r] << white_pieces["Pawn"]
-#             else
-#                 arr[r] << 0
-#             end
-#         end
-#     end
-#     return arr
-# end
